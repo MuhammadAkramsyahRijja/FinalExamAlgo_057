a3da8a1e1755bb205ef16e71472a150d1a0d5781
LENOVO <LENOVO@LAPTOP-MHHA1GU2>
2023-07-17 14:22:34 +0700

    Add project files.

---
+++ UASALGO.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//isi disini
+const int MAX_MAHASISWA = 100;
+int NIM[MAX_MAHASISWA];
+string nama[MAX_MAHASISWA];
+int tahunMasuk[MAX_MAHASISWA];
+int jumlahMahasiswa = 0;
+
+void tambahMahasiswa() {
+    if (jumlahMahasiswa < MAX_MAHASISWA) {
+        cout << "========== TAMBAH MAHASISWA ==========" << endl;
+        cout << "NIM: ";
+        cin >> NIM[jumlahMahasiswa];
+        cin.ignore();
+        cout << "Nama: ";
+        getline(cin, nama[jumlahMahasiswa]);
+        cout << "Tahun Masuk: ";
+        cin >> tahunMasuk[jumlahMahasiswa];
+        cin.ignore();
+        jumlahMahasiswa++;
+        cout << "Mahasiswa berhasil ditambahkan!" << endl;
+    }
+    else {
+        cout << "Kapasitas maksimum mahasiswa telah tercapai." << endl;
+    }
+}
+
+void tampilkanSemuaMahasiswa() {
+    cout << "========== DAFTAR MAHASISWA ==========" << endl;
+    for (int i = 0; i < jumlahMahasiswa; i++) {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM: " << NIM[i] << endl;
+        cout << "Nama: " << nama[i] << endl;
+        cout << "Tahun Masuk: " << tahunMasuk[i] << endl;
+        cout << "=====================================" << endl;
+    }
+}
+
+void algorithmacariMahasiswaByNIM() {
+    int nimCari;
+    cout << "========== CARI MAHASISWA BERDASARKAN NIM ==========" << endl;
+    cout << "Masukkan NIM yang ingin dicari: ";
+    cin >> nimCari;
+
+    bool ditemukan = false;
+    for (int i = 0; i < jumlahMahasiswa; i++) {
+        if (NIM[i] == nimCari) {
+            cout << "Mahasiswa dengan NIM " << nimCari << " ditemukan:" << endl;
+            cout << "Nama: " << nama[i] << endl;
+            cout << "Tahun Masuk: " << tahunMasuk[i] << endl;
+            ditemukan = true;
+            break;
+        }
+    }
+
+    if (!ditemukan) {
+        cout << "Mahasiswa dengan NIM " << nimCari << " tidak ditemukan." << endl;
+    }
+}
+
+void algorithmaSortByTahunMasuk() {
+    cout << "========== URUTKAN MAHASISWA BERDASARKAN TAHUN MASUK ==========" << endl;
+
+    // Lakukan pengurutan menggunakan algoritma sorting (misalnya bubble sort)
+    for (int i = 0; i < jumlahMahasiswa - 1; i++) {
+        for (int j = 0; j < jumlahMahasiswa - i - 1; j++) {
+            if (tahunMasuk[j] > tahunMasuk[j + 1]) {
+                // Tukar posisi data
+                swap(NIM[j], NIM[j + 1]);
+                swap(nama[j], nama[j + 1]);
+                swap(tahunMasuk[j], tahunMasuk[j + 1]);
+            }
+        }
+    }
+
+    cout << "Data mahasiswa telah diurutkan berdasarkan tahun masuk:" << endl;
+    for (int i = 0; i < jumlahMahasiswa; i++) {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM: " << NIM[i] << endl;
+        cout << "Nama: " << nama[i] << endl;
+        cout << "Tahun Masuk: " << tahunMasuk[i] << endl;
+        cout << "=====================================" << endl;
+    }
+}
+
+int main() {
+    int pilihan;
+    do {
+        cout << "========== MENU MANAJEMEN DATA MAHASISWA ==========" << endl;
+        cout << "1. Tambah Mahasiswa" << endl;
+        cout << "2. Tampilkan Semua Mahasiswa" << endl;
+        cout << "3. Cari Mahasiswa berdasarkan NIM" << endl;
+        cout << "4. Tampilkan Mahasiswa berdasarkan Tahun Masuk" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Pilihan: ";
+        cin >> pilihan;
+        cin.ignore();
+        switch (pilihan) {
+        case 1:
+            tambahMahasiswa();
+            break;
+        case 2:
+            tampilkanSemuaMahasiswa();
+            break;
+        case 3:
+            algorithmacariMahasiswaByNIM();
+            break;
+        case 4:
+            algorithmaSortByTahunMasuk();
+            break;
+        case 5:
+            cout << "Terima kasih! Program selesai." << endl;
+            break;
+        default:
+            cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
+        }
+        cout << endl;
+    } while (pilihan != 5);
+    return 0;
+}
+
+// 2.Algoritma penambahan mahasiswa (tambahMahasiswa()):
+//   Algoritma pencarian mahasiswa berdasarkan NIM (algorithmacariMahasiswaByNIM()):
+//   Algoritma pengurutan mahasiswa berdasarkan tahun masuk(algorithmaSortByTahunMasuk()) :
+// 3.Dengan perbedaan prinsip penyimpanan, 
+//   pengambilan, dan perilaku dalam mengakses elemen-elemen data tersebut, algoritma stack dan queue 
+//   dapat digunakan dalam berbagai situasi yang membutuhkan urutan penyimpanan dan pengambilan data yang berbeda.
+// 4.stack berguna Implementasi rekursi: Ketika suatu fungsi memanggil dirinya sendiri secara rekursif, stack digunakan 
+//   untuk menyimpan konteks pemanggilan fungsi tersebut. 
+// 5.
